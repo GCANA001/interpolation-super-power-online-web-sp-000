e748ad23d30ede5201f735c7b7379487c153506a
Glenn Canavan <gcana001@fiu.edu>
2019-03-20 02:12:53 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,7 @@
 def display_rainbow(color)
+color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
   puts "A:#{color[0]}, B: #{color[1]}, C: #{color[2]}, D:#{color[3]}, E:#{color[4]}, F:#{color[5]}, G:#{color[6]}, H:#{color[7]}"
-   puts color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+   
 end
 
  
