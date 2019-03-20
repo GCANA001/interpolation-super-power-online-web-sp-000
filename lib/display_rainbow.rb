68338adee2a5ef44ce4cf4ca5356efae8362808c
Glenn Canavan <gcana001@fiu.edu>
2019-03-20 02:24:11 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 def display_rainbow(color)
-  puts "R:#{color[0]}, O: #{color[1]}, Y: #{color[2]}, G:#{color[3]}, E:#{color[4]}, F:#{color[5]}, G:#{color[6]}, H:#{color[7]}"
+  puts "R:#{color[0]}, O: #{color[1]}, Y: #{color[2]}, G:#{color[3]}, B:#{color[4]}, I:#{color[5]}, V:#{color[6]}"
 end
 
 
