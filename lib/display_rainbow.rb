e2ebb72d3cf4cb28b1905b0bb3e0ed9e7c244c42
Glenn Canavan <gcana001@fiu.edu>
2019-03-20 01:57:37 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,11 @@
 def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  A = "red"
+  B = "orange"
+  C = "yellow"
+  D = "green"
+  E = "blue"
+  F = "indigo"
+  G = "violet"
   puts "A:#{0}, B: #{1}, C: #{2}, D:#{3}, E:#{4}, F:#{5}, G:#{6}, H:#{7}"
 end
   
